5c076eed7207a57eac6455162393505bb35b0fd8
David Carter <indysoftwaredev@gmail.com>
2025-03-13 08:47:10 -0400

    add unit test project

---
+++ DueDateCalculator.Tests/DueDateCalculatorTests.cs
@@ -0,0 +1,11 @@
+namespace DueDateCalculator.Tests
+{
+    public class DueDateCalculatorTests
+    {
+        [Fact]
+        public void TestFixtureIsWorking()
+        {
+            Assert.True(true);
+        }
+    }
+}
